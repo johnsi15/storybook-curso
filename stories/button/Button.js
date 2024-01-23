186d353f9ad6c908c25a5e0910b42acc5dfae40f
John Serrano <andrey_201015@hotmail.com>
2024-01-23 10:03:29 -0500

    Add styles button

--- stories/button/Button.js
+++ stories/button/Button.js
@@ -5,6 +5,7 @@ export const createButton = ({ style = 'outlined', size = 'small', label }) => {
   btn.type = 'submit'
   btn.role = 'button'
   btn.innerText = label
+  btn.className = ['button', `button--${size}`, `button--${style}`].join(' ')
   // btn.addEventListener('click', onClick)
 
   return btn
