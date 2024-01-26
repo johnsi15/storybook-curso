62a63be319d7e157dd2067accba7bc55d8c81048
John Serrano <andrey_201015@hotmail.com>
2024-01-26 11:05:50 -0500

    Add args with controls

--- stories/button/Button.stories.js
+++ stories/button/Button.stories.js
@@ -2,6 +2,25 @@ import { createButton } from './Button'
 
 export default {
   title: 'Design System/Atoms/Button',
+  argTypes: {
+    label: {
+      name: 'label',
+      control: {
+        type: 'text',
+      },
+    },
+    style: {
+      name: 'style',
+      options: ['filled', 'outlined'],
+      control: {
+        type: 'select',
+      },
+    },
+    size: {
+      control: 'select',
+      options: ['small', 'medium', 'large'],
+    },
+  },
 }
 
 const Template = ({ label, ...args }) => createButton({ label, ...args })
