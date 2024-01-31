7c2cb77609ebe37c08b251dabddca7066f20bda8
John Serrano <andrey_201015@hotmail.com>
2024-01-31 14:45:40 -0500

    Add Viewport, Measure y Outline

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,14 +1,36 @@
 /** @type { import('@storybook/html').Preview } */
+
+const customViewports = {
+  smallUgly: {
+    name: 'smallUgly',
+    styles: {
+      height: '540px',
+      width: '360px',
+    },
+  },
+  mediumUgly: {
+    name: 'mediumUgly',
+    styles: {
+      height: '1200px',
+      width: '1024px',
+    },
+  },
+}
+
 const preview = {
   parameters: {
-    actions: { argTypesRegex: "^on[A-Z].*" },
+    actions: { argTypesRegex: '^on[A-Z].*' },
     controls: {
       matchers: {
         color: /(background|color)$/i,
         date: /Date$/i,
       },
     },
+    viewport: {
+      viewports: customViewports,
+      defaultViewport: 'someDefault',
+    },
   },
-};
+}
 
-export default preview;
+export default preview
